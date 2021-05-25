192fdfede189067af84e7ffc101dc978a3ee691c
school01321 <80627249+school01321@users.noreply.github.com>
2021-05-25 12:09:34 +0900

    0525

---
+++ Assets/Scripts/Enermy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enermy : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rig;
+    public float speed = 25f;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(target.position, transform.position) < 10f)
+        {
+            Vector3 trans = target.position - transform.position;
+            rig.AddForce(trans * speed * Time.deltaTime);
+        }
+    }
+}
