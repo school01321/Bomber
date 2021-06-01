d2f03dfcbdece4431b6d0a21c9b31ca36552709a
school01321 <80627249+school01321@users.noreply.github.com>
2021-06-01 11:48:09 +0900

    0601

--- Assets/Scripts/Enermy.cs
+++ Assets/Scripts/Enermy.cs
@@ -7,6 +7,7 @@ public class Enermy : MonoBehaviour
     public Transform target;
     private Rigidbody rig;
     public float speed = 25f;
+    private bool scored = false;
 
     void Start()
     {
@@ -17,10 +18,21 @@ public class Enermy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(target.position, transform.position) < 10f)
+        if (Vector3.Distance(target.position, transform.position) < 10f)
         {
             Vector3 trans = target.position - transform.position;
             rig.AddForce(trans * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet" && scored == false)
+        {
+            scored = true; 
+            GameManager.instance.AddScore(1); 
+            transform.position = new Vector3(Random.Range(-30, 30), 0.5f, Random.Range(-30, 30)); 
+            scored = false;
         }
     }
 }
